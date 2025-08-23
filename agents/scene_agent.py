5dcfecb904f2a68c038ea4def80c52092e4138d7
Jun Han <junhanloh93@gmail.com>
2025-08-23 22:41:13 +0800

    working scene_agents

--- agents/scene_agent.py
+++ agents/scene_agent.py
@@ -1,20 +1,23 @@
+from typing import List
+from pydantic import BaseModel, Field
+import json
 prompt = '''
-Your outputs NEVER include double-quotes. 
-
 You are an advanced prompt-generation AI specializing in crafting highly detailed and hyper-realistic POV (point of view) image prompt ideas. Your task is to generate concise, action-driven, immersive prompt ideas that follow a sequential narrative, depicting a "day in the life" experience based on a given video topic in an actual location around the world. 
 
 Don't output actions related to wearing clothing.
 Don't output actions related to using feet.
-You also prioritize more sensational and scenic scenes for that given scenario, which common things people do generally
+You also prioritize more sensational and scenic scenes for that given scenario, which common things visitors do generally
 
-Guidelines:
-Every output represents a first-person perspective, making the viewer feel like they are experiencing the moment in the actual location with real context specific to the location.
-Use action-based verbs like gripping, running, reaching, holding, walking toward, stumbling, climbing, lifting, turning, stepping into.
-Use keywords such as POV, GoPro-style, first-person view, point of view to reinforce immersion.
-Keep all outputs between 5 to 10 words long.
-Never use double quotes in any output.
-All scenes must be hyper-realistic, based on real context specific to the location, high quality, and cinematic, evoking strong visual and emotional impact.
-Each set of prompts must follow a logical sequence, covering a full day in the life from morning to night, ensuring narrative continuity.
+<instruction>
+-Every output represents a first-person perspective, making the viewer feel like they are experiencing the moment in the actual location with real context specific to the location.
+-Use action-based verbs like gripping, running, reaching, holding, walking toward, stumbling, climbing, lifting, turning, stepping into.
+-Use keywords such as POV, GoPro-style, first-person view, point of view to reinforce immersion.
+-Keep all outputs between 6 to 15 words long.
+-Never use double quotes in any output.
+-All scenes must be hyper-realistic, based on real context specific to the location, high quality, and cinematic, evoking strong visual and emotional impact.
+-Always include the name of the location, landmark, activity, or place of interest in context
+-Each set of prompts must follow a logical sequence, covering a full day in the life from morning to night, ensuring narrative continuity.
+</instruction>
 
 Avoid introspection or vague descriptions—focus on physical actions and interactions that build a cohesive, immersive story.
 
@@ -27,7 +30,57 @@ Walking towards Marina Bay Sands at night along Singapores central business dist
 Topic: A tourist's day in Hotel Mount Fuji walking up to the Windows with a view to Mount Fuji. 
 Taking a bus towards Lake Kawaguchiko with a view to Mount Fuji.
 Admiring the scenic view of Chureito Pagoda with Mount Fuji in the backdrop.
-Taking a private onsen bath in Mount Fuji Hotle with a view of the Majestic Mount Fuji.
+Taking a private onsen bath in Mount Fuji Hotel with a view of the Majestic Mount Fuji.
 
 Each generated sequence tells a visual story about an actual location, pulling the viewer into a cohesive first-person experience from start to finish.
+
+Return the output in json
+class Scenes(BaseModel):
+    scenes: List[str]
 '''
+class Scenes(BaseModel):
+    scenes: List[str]
+
+class SceneAgent:
+    def __init__(self, client):
+        self.client = client
+        pass
+
+    def generate(self, msg):
+        response = self.client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": msg},
+            ],
+            response_format={
+                'type': 'json_object'
+            },
+            stream=False )
+        
+        # response = self.client.responses.parse(
+        #     model="deepseek-chat",
+        #     input=[
+        #         {
+        #             "role": "system",
+        #             "content": [
+        #                 {
+        #                     "type": "input_text",
+        #                     "text": prompt,
+        #                 }
+        #             ]
+        #         },
+        #         {
+        #             "role": "user",
+        #             "content": [
+        #                 {
+        #                     "type": "input_text",
+        #                     "text": msg,
+        #                 }
+        #             ]
+        #         }
+        #     ],
+        #     text_format=Scenes,
+        # )
+        
+        return json.loads(response.choices[0].message.content)
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from dotenv import load_dotenv
+from agents.scene_agent import SceneAgent
 load_dotenv()  # take environment variables
 import os
 from openai import OpenAI
-
+import pprint
+# python -m venv <myenv>
+# .\<myenv>\Scripts\activate
 def main():
-    ai_client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+    openai_client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+    deepseek_client = OpenAI(api_key=os.environ['DEEPSEEK_API_KEY'], base_url="https://api.deepseek.com")
 
+    scene_agent = SceneAgent( deepseek_client )
+    scenes = scene_agent.generate("Adventurer at Grand Canyon")
+    pprint.pprint(scenes)
+    
 if __name__ == "__main__":
     main()
